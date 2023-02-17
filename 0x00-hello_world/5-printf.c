35a974cc84abe6031c5c159de23642fe6a701b0e
rikoudo1er <maxwellwilliam121@gmail.com>
2023-02-16 17:11:53 -0800

    Add file 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -Entry point
+ * printf:	print string inside in output
+ * Return:	alway 0 (succes)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
