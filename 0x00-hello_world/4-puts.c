1790f6aba4e7c5e7951513d961fc27e5e4072011
rikoudo1er <maxwellwilliam121@gmail.com>
2023-02-16 17:00:03 -0800

    Add file 4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -Entry point
+ * puts:	print string inside in output
+ * Return:	alway 0 (succes)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
