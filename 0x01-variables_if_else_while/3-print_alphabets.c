675e94c209bd66a152065d081bafcacdcaf36f60
rikoudo1er <maxwellwilliam121@gmail.com>
2023-02-20 12:03:14 -0800

    Add file 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main -Entry point
+ * Return: alway 0 if the program run with succes
+ */
+int main(void)
+{	char letterLow = 'a';
+	char letterUper = 'A';
+
+	while (letterLow <= 'z')
+	{
+		putchar(letterLow);
+		letterLow++;
+	}
+	while (letterUper <= 'Z')
+	{
+		putchar(letterUper);
+		letterUper++;
+	}
+	printf("\n");
+
+	return (0);
+}
