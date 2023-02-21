886211f3f586f7fcccb16c3e8ffcea8a7da4d4da
rikoudo1er <maxwellwilliam121@gmail.com>
2023-02-20 19:13:16 -0800

    Add file 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main -Entry point
+ * Return: alway 0 if the program run with succes
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+				if ((i == 7) && (j == 8) && (k == 9))
+				break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+	}
+	putchar('\n');
+
+	return (0);
+}
