efa9636a4cf104f583dc2a11bcde6fec8dcc41e3
rikoudo1er <maxwellwilliam121@gmail.com>
2023-02-20 19:03:46 -0800

    Add file 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,12 +16,10 @@ int main(void)
 	{
 		for (j = i + 1; j <= 9; j++)
 		{
-			
 			putchar('0' + i);
 			putchar('0' + j);
 			if ((i == 8) && (j == 9))
-				break;
-		
+			break;
 			putchar(',');
 			putchar(' ');
 		}
