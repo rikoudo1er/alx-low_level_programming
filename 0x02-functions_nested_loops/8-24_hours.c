e82edfdcd06bba04614e9a30819eeb4f9121538d
rikoudo1er <maxwellwilliam121@gmail.com>
2023-02-22 19:00:43 -0800

    Add file 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * jack_bauer -print every minute of the day,starting from 00:00 to 23:59.
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		if (i < 2)
+		{
+			for (j = 0; j <= 9; j++)
+			{
+				for (k = 0; k <= 5; k++)
+					for (l = 0; l <= 9; l++)
+					{
+						_putchar('0' + i);
+						_putchar('0' + j);
+						_putchar(':');
+						_putchar('0' + k);
+						_putchar('0' + l);
+						_putchar('\n');
+					}
+			}
+		}
+
+		if (i == 2)
+		{
+			for (j = 0; j <= 3; j++)
+			{
+				for (k = 0; k <= 5; k++)
+					for (l = 0; l <= 9; l++)
+					{
+						_putchar('0' + i);
+						_putchar('0' + j);
+						_putchar(':');
+						_putchar('0' + k);
+						_putchar('0' + l);
+						_putchar('\n');
+					}
+			}
+		}
+
+	}
+}
