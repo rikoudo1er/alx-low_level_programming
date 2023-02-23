47b4096701b99326d9cbf442622b5d6026a3223e
rikoudo1er <maxwellwilliam121@gmail.com>
2023-02-22 20:28:59 -0800

    Add file 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add -return the sum of two number
+ * @num1: first number
+ * @num2: last number
+ * Return: sum of @num1 and @num2
+ */
+
+int add(int num1, int num2)
+{
+
+	return (num1 + num2);
+}
