1784555386fb276db7fe331e6413c39d33e8eeb2
rikoudo1er <maxwellwilliam121@gmail.com>
2023-02-24 18:37:42 -0800

    Add file 8

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,15 +8,20 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n > 0)
 	{
 	for (i = 0; i < n; i++)
-	{
+	{	
+		for(j = 2 + i; j < n; j++)
+			_putchar(' ');
+
 
 		_putchar('\\');
 		_putchar('\n');
+	
+
 	}
 	_putchar('\n');
 	}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square- print a square with the character '#'.
+ * @size: is the size of the square.
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+
+		}
+	}
+	else if (size <= 0)
+		_putchar('\n');
+}
