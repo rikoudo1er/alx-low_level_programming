7bce49ff6b17dc5a494b4d4ffd4bf89a9528f220
rikoudo1er <maxwellwilliam121@gmail.com>
2023-02-24 17:52:33 -0800

    Add file 5 edited

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * more_numbers- prints 10 times the numbers, from 0 to 1.
@@ -9,15 +9,25 @@
 void more_numbers(void)
 {
 	int i, j;
+	char ret = '\n';
+	char c1, c2;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			printf("%d", j);
+			if(j < 10)
+				c2 = '0' + j;
+
+			else
+			{
+				c1 = '0' + j / 10;
+				c2 = '0' + j % 10;
+				_putchar(c1);
+			}
+			_putchar(c2);
 		}
-		_putchar('\n');
+		_putchar(ret);
 	}
 
-	_putchar('\n');
 }
