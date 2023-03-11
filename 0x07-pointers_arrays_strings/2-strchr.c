dc130e947af838fb6c2d21e3c0c0620a97d7c9cc
rikoudo1er <maxwellwilliam121@gmail.com>
2023-03-11 04:30:24 -0800

    Add file 2 edited

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 
 /**
@@ -16,13 +17,14 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
-		{
 			return (s);
-		}
 
 		++s;
 	}
 
-	return (0);
+	if (*s == c)
+		return (s);
+
+	return (NULL);
 
 }
