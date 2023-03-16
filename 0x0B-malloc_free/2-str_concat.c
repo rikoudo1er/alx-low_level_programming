3ab719a63c1c9c1abef810dab6bf05c59d393605
rikoudo1er <maxwellwilliam121@gmail.com>
2023-03-15 20:03:46 -0700

    Add file 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1  == NULL)
 		s1 = "";
-	else if (s2 == NULL)
+	if (s2 == NULL)
 		s2 = "";
 
 	str = (char *)malloc(((strlen(s1) + strlen(s2)) + 1) * sizeof(char));
