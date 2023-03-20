cba8b88fb13ed8601ed2e1d7b7ef687ec6658041
rikoudo1er <maxwellwilliam121@gmail.com>
2023-03-20 14:39:23 -0700

    add dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef __DOG__H
+#define __DOG__H
+
+/**
+ * struct dog - basic structures describing a dog
+ * @name: the name of dog
+ * @age: age of dog
+ * @owner:owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
