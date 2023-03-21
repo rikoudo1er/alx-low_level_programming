9ce9936f35ad51ac0841a89cd0f6f76c56ea8e6a
rikoudo1er <maxwellwilliam121@gmail.com>
2023-03-20 19:16:55 -0700

    add 5-free_dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free memories used by the dog (yes :) it's sounds creazy)
+ * @d: struct dog
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
