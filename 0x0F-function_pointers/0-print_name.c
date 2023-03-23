e77cb4686c7d66e246d54254ca03410a3bb896ab
rikoudo1er <maxwellwilliam121@gmail.com>
2023-03-23 13:49:40 -0700

    add 0-print_name edited

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
  * print_name - print a name
@@ -9,7 +10,7 @@
 void print_name(char *name, void (*f)(char *))
 {
 
-	if (name)
+	if (name != NULL && f != NULL)
 	{
 		(*f)(name);
 	}
