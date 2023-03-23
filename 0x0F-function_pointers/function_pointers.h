a0f288e2bd57b93abf7fd2bdaabd30b4232e8ecd
rikoudo1er <maxwellwilliam121@gmail.com>
2023-03-23 14:16:16 -0700

    update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef __FUNC_PONTERS__H
 #define __FUNC_PONTERS__H
-
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
