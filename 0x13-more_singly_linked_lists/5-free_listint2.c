082bbfee2d3a5b9402dd8fd83efb96581e4e6f21
rikoudo1er <maxwellwilliam121@gmail.com>
2023-04-05 18:00:16 -0700

    add 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free list
+ * @head:node;
+ */
+
+void free_listint2(listint_t **head)
+{
+
+	listint_t *node, *current_node;
+
+	if (*head)
+	{
+		current_node = *head;
+		while (current_node != NULL)
+		{
+			node = current_node;
+			current_node = current_node->next;
+			free(node);
+		}
+		free(current_node);
+		*head = NULL;
+	}
+
+}
