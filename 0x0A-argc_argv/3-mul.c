9b21f60c7bf93ff8f5cb6bc9cab76a3734ea71e5
rikoudo1er <maxwellwilliam121@gmail.com>
2023-07-15 18:42:10 -0700

    add files 1,2,3and 4

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- prints all arguments it receives.
+ * @argc: size of @argv.
+ * @argv: an array hold the command line arguments.
+ * Return: alway 0.
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int sum, arg1, arg2;
+
+	if (argc == 3)
+	{
+		arg1 = atoi(argv[1]);
+		arg2 = atoi(argv[2]);
+		sum = arg1 * arg2;
+
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (EXIT_SUCCESS);
+
+
+}
+
